11e8f76e8ed3ef1115742ba9a63fb1d660572e0e
zawztun <zzt.nyc@gmail.com>
2023-04-20 10:55:30 -0400

    vercel analytics added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
         <Component {...pageProps} />
+
+        <Analytics />
       </div>
     </>
   );
