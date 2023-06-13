6679721b5f368436898d2685755337d78c1dc912
zawztun <zzt.nyc@gmail.com>
2023-06-13 18:26:42 -0400

    fixed react-icon err

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import Link from "next/link";
-import Image from "next/image";
+
 import Modal from "./Modal";
-import { AiOutlineBars } from "react-icons/Ai";
+import { AiOutlineBars } from "react-icons/ai";
 
 type Props = {};
 
