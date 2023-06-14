0d9647b315a4f853c1f5bf2af09dd6d76bcbdf77
zawztun <zzt.nyc@gmail.com>
2023-06-14 14:15:35 -0400

    updateed bg-gradient

--- src/components/gradient/Gradient.tsx
+++ src/components/gradient/Gradient.tsx
@@ -5,7 +5,7 @@ type Props = {};
 const Gradient = (props: Props) => {
   return (
     <>
-      <div className="w-[320px] h-[350px] aspect-video absolute right-20 -top-30  bg-gradient-to-br from-pink-400 to-sky-600 rounded-full blur-2xl opacity-30"></div>
+      <div className="w-[420px] h-[250px] aspect-video absolute right-20 top-20 bg-gradient-to-br from-pink-400 to-sky-600 rounded-full blur-2xl opacity-30"></div>
     </>
   );
 };
--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -8,7 +8,7 @@ type Props = {};
 
 const Hero = (props: Props) => {
   return (
-    <div className="relative w-full py-8 pb-6 mx-auto overflow-x-auto text-white bg-gray-800">
+    <div className="relative w-full py-8 pb-6 mx-auto text-white bg-gray-800">
       <Gradient />
       <div className="flex flex-col-reverse items-center justify-center w-full gap-4 mx-auto lg:w-3/5 lg:mx-auto lg:flex-row lg:justify-between">
         <div className="flex flex-col gap-8 p-2 duration-1000 animate-fade-in-down scrollbar">
